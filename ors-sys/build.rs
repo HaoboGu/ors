64aecd5266678df6bf17e7a8b56912a2a2ca4072
Haobo Gu <haobo.haobogu@alibaba-inc.com>
2021-11-16 10:41:28 +0800

    refactor: move ors high-level api to ors package

--- ors-sys/build.rs
+++ ors-sys/build.rs
@@ -18,6 +18,8 @@ fn main() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Format bindings using rustfmt
+        .rustfmt_bindings(true)
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
