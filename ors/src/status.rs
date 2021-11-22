1d263d94af86b13f5c3c4dcfe05a8cbd5359f64c
HaoboGu <haobogu@outlook.com>
2021-11-22 21:11:44 +0800

    fix(ci): fix typo

--- ors/src/status.rs
+++ ors/src/status.rs
@@ -1,10 +1,9 @@
 use std::{
     ffi::{CStr, CString},
-    ops::DerefMut,
     sync::{atomic::AtomicPtr, Arc, Mutex},
 };
 
-use lazy_static::{__Deref, lazy_static};
+use lazy_static::{lazy_static};
 use ors_sys::*;
 lazy_static! {
     static ref API: Arc<Mutex<AtomicPtr<OrtApi>>> = {
