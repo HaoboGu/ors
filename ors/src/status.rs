7f97f41b4825ebf07bd5986a015c3c0aec2ced38
HaoboGu <haobogu@outlook.com>
2021-11-22 21:33:32 +0800

    feat(ors): add api entry for ors, add status api

--- ors/src/status.rs
+++ ors/src/status.rs
@@ -11,13 +11,18 @@ fn get_error_code(status: *const OrtStatus) -> OrtErrorCode {
     return unsafe { get_api().GetErrorCode.unwrap()(status) };
 }
 
+fn release_status(status: *mut OrtStatus) {
+    unsafe { get_api().ReleaseStatus.unwrap()(status) }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_ort_status() {
         let status = create_status(OrtErrorCode_ORT_MODEL_LOADED, "OKOKOKO".to_string());
         println!("error code: {}", get_error_code(status));
+        release_status(status as *mut OrtStatus);
     }
 }
