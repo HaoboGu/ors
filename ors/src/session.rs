bb310e17ce6f78560b083f08e6d7b1046ed9e64c
Haobo Gu <haobo.haobogu@alibaba-inc.com>
2021-12-13 18:21:32 +0800

    feat: [WIP]add session builder

--- ors/src/session.rs
+++ ors/src/session.rs
@@ -1,9 +1,81 @@
 use ors_sys::*;
+use std::path::Path;
 use std::{ffi::CString, ptr::null_mut};
+use anyhow::Result;
 
 use crate::api::get_api;
-use crate::call_ort;
+use crate::{call_ort, OptimizationLevel};
 use crate::status::check_status;
+
+pub struct Session {
+	session_ptr: *mut OrtSession
+}
+
+pub struct SessionBuilder {
+	session_options_ptr: *mut OrtSessionOptions,
+}
+
+impl SessionBuilder {
+	pub fn new() -> Result<Self> {
+		let allocator_type = OrtAllocatorType_OrtArenaAllocator;
+		let mem_type = OrtMemType_OrtMemTypeDefault;
+		let mut session_options_ptr: *mut OrtSessionOptions = null_mut();
+		let status = call_ort!(CreateSessionOptions, &mut session_options_ptr);
+		check_status(status)?;
+
+		Ok(SessionBuilder {
+			session_options_ptr,
+		})
+	}
+
+	pub fn build_with_model_from_file<P>(self, model_filepath: P) -> Result<Session> where 
+    P: AsRef<Path> {
+        let filepath = model_filepath.as_ref();
+        todo!();
+    }
+
+    pub fn build_with_model_in_momory(self, model_bytes: &[u8]) -> Result<Session> {
+        todo!();
+    }
+
+	/// Configure the session to use a number of threads
+	pub fn intra_number_threads(self, num_threads: i32) -> Result<SessionBuilder> {
+		let status = call_ort!(SetIntraOpNumThreads, self.session_options_ptr, num_threads);
+		check_status(status)?;
+		Ok(self)
+	}
+
+    pub fn inter_number_threads(self, num_threads: i32) -> Result<SessionBuilder> {
+        todo!();
+    }
+
+	/// Set the session's optimization level
+	pub fn graph_optimization_level(
+		self,
+		opt_level: OptimizationLevel,
+	) -> Result<SessionBuilder> {
+		// Sets graph optimization level
+		let status = call_ort!(SetSessionGraphOptimizationLevel, self.session_options_ptr, opt_level.into());
+		check_status(status)?;
+		Ok(self)
+	}
+
+
+    /// Set execution mode
+    /// TODO: Wrap ExecutionMode
+    pub fn execution_mode(self, execution_mode: i32) -> Result<SessionBuilder> {
+        todo!();
+    }
+
+    pub fn cpu_mem_arena_enabled(self, cpu_mem_arena_enabled: bool) -> Result<SessionBuilder> {
+        todo!();
+    }
+
+    pub fn mem_pattern_enabled(self, mem_pattern_enabled: bool) -> Result<SessionBuilder> {
+        todo!();
+    }
+
+}
 
 pub(crate) fn create_session(
     env: *const OrtEnv,
