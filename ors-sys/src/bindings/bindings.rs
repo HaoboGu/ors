324789f8d3d6b87d4b6cc33bfedbf76c7b641167
Haobo Gu <haobogu@outlook.com>
2021-12-31 14:47:19 +0800

    feat(sys): add dynamic bindings for linux

--- ors-sys/src/bindings/bindings.rs
+++ ors-sys/src/bindings/bindings.rs
@@ -26,8 +26,16 @@ include!(concat!(
     "/src/bindings/macos/x86_64/bindings_dynamic.rs"
 ));
 
+#[cfg(not(feature = "runtime-linking"))]
 #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 include!(concat!(
     env!("CARGO_MANIFEST_DIR"),
     "/src/bindings/linux/x86_64/bindings.rs"
+));
+
+#[cfg(feature = "runtime-linking")]
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+include!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/src/bindings/linux/x86_64/bindings_dynamic.rs"
 ));
